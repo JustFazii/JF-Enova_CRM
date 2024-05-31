d285bc7a85a8fbca789af8a926c546d8873925be
Mikołaj Trawkowski <MJTrawkowski@outlook.com>
2024-05-31 16:45:10 +0200

    Added sorting by input into goods page, fixed some comments, fixed formatting

--- ui/js/goods.js
+++ ui/js/goods.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   if (window.location.pathname.endsWith("goods.html")) {
-    eel.show_goods()(function(data) {
-        show_goods(data);
-    })
+    eel.show_goods()(function (data) {
+      show_goods(data);
+    });
   }
 });
 
 eel.expose(show_goods);
 function show_goods(data) {
   document.getElementById("output3").innerHTML = data;
+  const search = document.querySelector(".input-group input"),
+    table_rows = document.querySelectorAll("tbody tr");
+
+  search.addEventListener("input", searchTable);
+
+  function searchTable() {
+    table_rows.forEach((row, i) => {
+      let table_data = row.textContent,
+        search_data = search.value;
+
+      row.classList.toggle("hide", table_data.indexOf(search_data) < 0);
+      row.style.setProperty("--delay", i / 25 + "s");
+    });
+  }
 }
--- ui/js/output.js
+++ ui/js/output.js
@@ -1,54 +1,40 @@
 eel.expose(update_output2);
 function update_output2(data) {
   document.getElementById("output2").innerHTML = data;
-  const search = document.querySelector('.input-group input'),
-    table_rows = document.querySelectorAll('tbody tr');
+  const search = document.querySelector(".input-group input"),
+    table_rows = document.querySelectorAll("tbody tr");
 
-search.addEventListener('input', searchTable);
+  search.addEventListener("input", searchTable);
 
-console.log(search)
-console.log(table_rows)
-
-function searchTable() {
-    table_rows.forEach( (row,i )=> {
-        let table_data = row.textContent,
+  function searchTable() {
+    table_rows.forEach((row, i) => {
+      let table_data = row.textContent,
         search_data = search.value;
-        
-        console.log(table_data);
-        console.log(search_data);
 
-        console.log(table_data.indexOf(search_data) < 0)
-        row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
-        row.style.setProperty('--delay', i/25 + 's')
-    })
-}
+      row.classList.toggle("hide", table_data.indexOf(search_data) < 0);
+      row.style.setProperty("--delay", i / 25 + "s");
+    });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   if (window.location.pathname.endsWith("contractors.html")) {
-    eel.refresh_contractors()(function(data) {
+    eel.refresh_contractors()(function (data) {
       update_output2(data);
-        const search = document.querySelector('.input-group input'),
-      table_rows = document.querySelectorAll('tbody tr');
+      const search = document.querySelector(".input-group input"),
+        table_rows = document.querySelectorAll("tbody tr");
 
-  search.addEventListener('input', searchTable);
+      search.addEventListener("input", searchTable);
 
-  console.log(search)
-  console.log(table_rows)
-
-  function searchTable() {
-      table_rows.forEach( (row,i )=> {
+      function searchTable() {
+        table_rows.forEach((row, i) => {
           let table_data = row.textContent,
-          search_data = search.value;
-          
-          console.log(table_data);
-          console.log(search_data);
+            search_data = search.value;
 
-          console.log(table_data.indexOf(search_data) < 0)
-          row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
-          row.style.setProperty('--delay', i/25 + 's')
-      })
-  }
+          row.classList.toggle("hide", table_data.indexOf(search_data) < 0);
+          row.style.setProperty("--delay", i / 25 + "s");
+        });
+      }
     });
   }
 });
@@ -57,8 +43,8 @@ if (localStorage.getItem("theme") === "dark") {
   document.documentElement.classList.add("dark-theme");
 }
 
-// document.onkeydown = (e) => {
-//   if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-//       e.preventDefault();
-//   }
-// };
+document.onkeydown = (e) => {
+  if (e.ctrlKey && e.shiftKey && e.key == "I") {
+    e.preventDefault();
+  }
+};
