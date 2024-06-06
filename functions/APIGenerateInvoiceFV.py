2b1f150f24dcea480f8bc558a8acda785f12e6fe
Mikołaj Trawkowski <MJTrawkowski@outlook.com>
2024-06-06 17:07:05 +0200

    I think, this is final commit

--- functions/APIGenerateInvoiceFV.py
+++ functions/APIGenerateInvoiceFV.py
@@ -1,13 +1,13 @@
 import requests
-
+from functions.env_file import TOKEN_ENOVA, IP
 class APIZOFVInvoice:
     def __init__(self):
-        self.base_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdGgtbXRoIjoiMTAyNCIsImF0aC1wd3QiOiIxIiwiYXRoLWRiIjoiSU5GXzIyNDMyIiwiYXRoLWd1aWQiOiIzNmQ1NGRmYy04N2YyLTQ5YWEtYTRiMi1kM2FjZDhiZTY5YzYiLCJhdGgtc3ZjIjoiQXBpIiwibmJmIjoxNzE2NTU2NzY3LCJleHAiOjE3NDgwOTI3NjcsImlhdCI6MTcxNjU1Njc2NywiaXNzIjoiaHR0cHM6Ly93d3cuZW5vdmEucGwiLCJhdWQiOiJlbm92YTM2NSJ9.k4SbzPbPheoNn7CajBhWj0CzECeb5HzIEvSzahyeC3TaPiIcdxbnlijKZp2loe6JXu1z62V2pS4erLMpOBUUFrSnbet36iZpcXWGbL69VI3GCSHnwRcD1ssMYMxp6pKTZf0OdRfKlfyFeI4ntZdzh2vs7aBFhaHE0m5Wv3EpH3Biv9dVMACu6ayAthg4Of6vSMRfr7T_MZDN35aCYMsWordxlWUJwJ5K3YrpjfLAQXNLEsjfkv1dcqHY3NZtj2c1Z84PApRpIeo1kjJLvV5-7UIt7xX1cPMuC3omZfhEmUAuYe31YMHiLhb7QThx9Zcd33Tq0fYWPLe-pzVLelrthg"
+                self.base_token = TOKEN_ENOVA
 
 
     def send_request(self, fv):
-        login_url = "http://192.168.0.23:6001/api/LoginApi"
-        add_fv_url = f"http://192.168.0.23:6001/api/ServiceImpApiANS/GenerateInvoice?idDokumentuZO={fv['idDokumentuZO']}&typDokumentu=FV"
+        login_url = f"http://{IP}:6001/api/LoginApi"
+        add_fv_url = f"http://{IP}:6001/api/ServiceImpApiANS/GenerateInvoice?idDokumentuZO={fv['idDokumentuZO']}&typDokumentu=FV"
 
         headers = {
             'Authorization': f'Bearer {self.base_token}',
